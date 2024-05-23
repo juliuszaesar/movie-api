9b162a036872321ceeff2b113a722b71e7785e05
Julius Naski <julius.naski@gmail.com>
2024-05-23 14:45:37 +0200

    custom errors and restricting inputs

--- cmd/api/helpers.go
+++ cmd/api/helpers.go
@@ -3,8 +3,11 @@ package main
 import (
 	"encoding/json"
 	"errors"
+	"fmt"
+	"io"
 	"net/http"
 	"strconv"
+	"strings"
 
 	"github.com/julienschmidt/httprouter"
 )
@@ -38,5 +41,67 @@ func (app *application) writeJSON(w http.ResponseWriter, status int, data envelo
 	w.WriteHeader(status)
 	w.Write(js)
 
+	return nil
+}
+
+func (app *application) readJSON(w http.ResponseWriter, r *http.Request, dst any) error {
+	// Set max byte size for request body to 1mb.
+	maxBytes := 1_048_576
+	r.Body = http.MaxBytesReader(w, r.Body, int64(maxBytes))
+
+	// Initialize json.Decoder then use DisallowUnknownField() method, client can therefore not send field that
+	// cannot be mapped to target destination, instead returns an error. Also securing against Man-Middle-Attacks.
+	dec := json.NewDecoder(r.Body)
+	dec.DisallowUnknownFields()
+
+	// Decode req body to target destination
+	err := dec.Decode(dst)
+	if err != nil {
+		var syntaxError *json.SyntaxError
+		var unmarshalTypeError *json.UnmarshalTypeError
+		var invalidUnmarshalError *json.InvalidUnmarshalError
+
+		var maxBytesError *http.MaxBytesError
+
+		switch {
+		case errors.As(err, &syntaxError):
+			return fmt.Errorf("body contains incorrect formed json at character %d", syntaxError.Offset)
+
+		case errors.Is(err, io.ErrUnexpectedEOF):
+			return errors.New("body contains incorrect formed json")
+
+		case errors.As(err, &unmarshalTypeError):
+			if unmarshalTypeError.Field != "" {
+				return fmt.Errorf("body contains incorrect json type for field %q", unmarshalTypeError.Field)
+			}
+			return fmt.Errorf("body contains incorrect json type at character %d", unmarshalTypeError.Offset)
+
+		case errors.Is(err, io.EOF):
+			return errors.New("body cannot be empty")
+
+		// If JSON contains a field that cannot be mapped to target dest, an error message is returned,
+		// It checks for this, extracts field name from error, inserts into custom message.
+		case strings.HasPrefix(err.Error(), "json: unknown field "):
+			fieldName := strings.TrimPrefix(err.Error(), "json: unknown field ")
+			return fmt.Errorf("body contains unknown field key %s", fieldName)
+
+		// Check if error has exceeded max byte size if type is *http.MaxBytesError.
+		// Returns an error message
+		case errors.As(err, &maxBytesError):
+			return fmt.Errorf("body cannot be longer than %d bytes", maxBytesError.Limit)
+
+		case errors.As(err, &invalidUnmarshalError):
+			panic(err)
+
+		default:
+			return err
+		}
+	}
+
+	// If req body is single JSON value, return io.EOF. If error, it means that it's more than a single JSON value.
+	err = dec.Decode(&struct{}{})
+	if !errors.Is(err, io.EOF) {
+		return errors.New("body contains more than a single JSON value")
+	}
 	return nil
 }
